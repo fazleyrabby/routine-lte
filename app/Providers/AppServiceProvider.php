5ee98330923392b28e0ebff19ae360f7d1c1f337
Md. Fazley Rabbi <fazley111@gmail.com>
2024-02-24 22:35:30 +0600

    minor changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+        
         view()->composer('*', function ($view)
         {
             $requests = '';
